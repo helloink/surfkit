d049e5bab203e9ddaa9f043ccf60cf27f3cd8fa9
Andreas Wolff <aw@heimathafen.co>
2019-08-22 15:42:23 +0200

    bump 1.2.0

--- surfkit.go
+++ surfkit.go
@@ -11,7 +11,7 @@ import (
 	"github.com/helloink/surfkit/events"
 )
 
-const version = "1.2.0-d"
+const version = "1.2.0"
 
 // Output defines the single channel on which the service produces output, given it is a Pubsub output.
 // Eventuall this should also cover HTTP Endpoints.
