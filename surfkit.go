b0c13b5259b2151701927898b535b7740d2b0c09
Levin Fritz <levin.fritz@gmail.com>
2020-05-17 13:50:50 +0200

    Bump version and update changelog

--- surfkit.go
+++ surfkit.go
@@ -6,7 +6,7 @@ import (
 	"github.com/helloink/surfkit/events"
 )
 
-const version = "1.9.0"
+const version = "1.9.1"
 
 // Output defines the single channel on which the service produces output, given it is a Pubsub output.
 // Eventually this should also cover HTTP Endpoints.
